e9fbc5e4c1cd91d5335d62a40266ec9866cf7ac0
sharmainne Campo <Sharmainnecampo30@gmail.com>
2025-07-16 15:09:04 +0800

    Update Motor.h

--- Source of Program/library/Motor/Motor.h
+++ Source of Program/library/Motor/Motor.h
@@ -1,1 +1,21 @@
- huh
+ #ifndef MOTOR_H
+#define MOTOR_H
+
+class Motor {
+    public: 
+        Motor(int pin1, int pin2);
+        Motor(int pin1, int pin2, int deadzone);
+        void setup(int pin1, int pin2);
+        void write(int speed);
+        int read();
+        void reset();
+        void deadzone(int deadzone);
+
+    private:
+        int pin1;  // Pin for motor control
+        int pin2;  // Second pin for motor control
+        int last_speed = 0; // Track last written speed
+        int zone;
+};
+
+#endif // !MOTOR_H
