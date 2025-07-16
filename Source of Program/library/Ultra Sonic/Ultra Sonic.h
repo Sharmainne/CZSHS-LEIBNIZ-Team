62c59f8af53c009915cbd394bb44ec290548050d
sharmainne Campo <Sharmainnecampo30@gmail.com>
2025-07-16 15:12:42 +0800

    Update Ultra Sonic.h

--- Source of Program/library/Ultra Sonic/Ultra Sonic.h
+++ Source of Program/library/Ultra Sonic/Ultra Sonic.h
@@ -1,1 +1,36 @@
-HAHEDUAHAGEGSHAHSHS
+#ifndef ULTRA_SONIC_H
+#define ULTRA_SONIC_H
+
+#include <Arduino.h>
+#include <NewPing.h>
+
+class Ultra_Sonics {
+    public:
+        NewPing mySonar1;
+        NewPing mySonar2;
+        NewPing mySonar3;
+        NewPing mySonar4;
+        long sonar1;
+        long sonar2;
+        long sonar3;
+        long sonar4;
+        int maxDistance = 200;
+
+        Ultra_Sonics(int max_distance,  int trigPin1, int echoPin1, 
+                                        int trigPin2, int echoPin2, 
+                                        int trigPin3, int echoPin3, 
+                                        int trigPin4, int echoPin4);
+
+        void read();  
+        void read(int delayms); 
+        void maxDist(int max);
+        bool allResponding();
+        long getFront();
+        long getSide();
+        long getLeft();
+        long getFrontLeft();
+        long getFrontRight();
+        long getRight();
+};
+
+#endif // !ULTRA_SONIC_H
